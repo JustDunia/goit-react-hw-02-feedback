0ee347d7d85f4873a3886c9b9dbe3593dbdfef1c
Justyna <justyna.sienkiewicz93@gmail.com>
2023-05-18 20:04:47 +0200

    added proptypes and basic styling

--- src/components/FeedbackOptions.js
+++ src/components/FeedbackOptions.js
@@ -1,20 +1,39 @@
 import { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import styles from './FeedbackOptions.module.css';
 
 export class FeedbackOptions extends Component {
   render() {
     const { options, onLeaveFeedback } = this.props;
     return (
       <div>
-        <button onClick={onLeaveFeedback} id={options[0]}>
+        <button
+          className={styles.btn}
+          onClick={onLeaveFeedback}
+          id={options[0]}
+        >
           Good
         </button>
-        <button onClick={onLeaveFeedback} id={options[1]}>
+        <button
+          className={styles.btn}
+          onClick={onLeaveFeedback}
+          id={options[1]}
+        >
           Neutral
         </button>
-        <button onClick={onLeaveFeedback} id={options[2]}>
+        <button
+          className={styles.btn}
+          onClick={onLeaveFeedback}
+          id={options[2]}
+        >
           Bad
         </button>
       </div>
     );
   }
 }
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
